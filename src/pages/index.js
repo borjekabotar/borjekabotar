3a4f15aa2f12a6d87f37da28f9b12d38e970653b
ddomizia <domizia.derasmo@gmail.com>
2022-04-06 19:54:29 +0200

    test-seo-index

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const index = () => {
         title="Borj-e Kabotar | Home"
         description="Borj–e Kabotar is a project born to focus on architecture and anthropology of the pigeons towers in the Isfahan province and to study these imposing buildings with a diameter that can reach 20 meters and a height that touches 15 m."
         url="https://www.borjekabotar.com/"
-        image={withPrefix(`/images/sfondo_bk.jpg`)}
+        image={`${withPrefix(`/images/sfondo_bk.jpg`)}`}
       >
         <link rel="shortcut icon" href={withPrefix(`/favicon/favicon.ico`)} />
         <link
